49d729edc78659e1f2a1be956ada84a39dd26205
Marat1Kh <336199@niuitmo.ru>
2025-02-24 15:19:20 +0300

    installed dependencies

---
+++ src/components/Footer.tsx
@@ -0,0 +1,6 @@
+export const Footer = () => {
+    return ( <p className="text-gray-400 text-sm">
+    Footer content
+</p>
+)
+};
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,8 @@
+"use client";
+
+export const Navbar = () => {
+    return ( <p className="text-gray-400 text-sm">
+    Navbar
+</p>
+)
+};
